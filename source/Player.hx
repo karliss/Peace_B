2c59c648cf85b78f7f417a47490f9945aee33e11
Kārlis Seņko <karlis3p70l1ij@gmail.com>
2019-01-26 14:11:14 +0200

    Make it easier to place walls while standing still.

--- source/Player.hx
+++ source/Player.hx
@@ -10,7 +10,8 @@ enum Direction {
 
 class Player extends AnimatedSprite {
 	public var moveDirection:FlxPoint = new FlxPoint(0, 0);
-	public var lastDir:Direction = Direction.Here;
+	public var lastMoveDir:Direction = Direction.Right;
+	public var currentDir:Direction = Direction.Here;
 	public var object:PlayState.PickedObject;
 
 	public function new() {
@@ -23,7 +24,10 @@ class Player extends AnimatedSprite {
 	}
 
 	public function addMove(d:Direction) {
-		this.lastDir = d;
+		this.currentDir = d;
+		if (d != Direction.Here) {
+			lastMoveDir = d;
+		}
 		switch (d) {
 			case Left:
 				moveDirection.x -= maxVelocity.x * 4;
@@ -39,21 +43,19 @@ class Player extends AnimatedSprite {
 
 	override public function update(d:Float) {
 		acceleration.set(moveDirection.x, moveDirection.y);
-		lastDir = Here;
+		currentDir = Here;
 		super.update(d);
 		this.moveDirection.set(0, 0);
 	}
 
 	public function pickupPlayer(state:PlayState) {
-		pickup(lastDir, state);
+		pickup(currentDir, state);
 	}
 
 	public function pickup(direction:Direction, state:PlayState) {
-		trace("foo1");
 		if (object != null) {
 			return;
 		}
-		trace("foo2");
 		var pos = this.getMidpoint();
 		var x:Int = Math.floor(pos.x / 32);
 		var y:Int = Math.floor(pos.y / 32);
@@ -72,7 +74,7 @@ class Player extends AnimatedSprite {
 	}
 
 	public function placePlayer(state:PlayState) {
-		return this.place(lastDir, state);
+		return this.place(lastMoveDir, state);
 	}
 
 	public function place(direction:Direction, state:PlayState) {
