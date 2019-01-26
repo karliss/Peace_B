cca9ed19973aa86092ff7801f558e34f32efda0b
Kārlis Seņko <karlis3p70l1ij@gmail.com>
2019-01-26 09:46:37 +0200

    Add main menu.

---
+++ source/ButtonMenu.hx
@@ -0,0 +1,84 @@
+package;
+
+import flixel.FlxState;
+import flixel.FlxSprite;
+import flixel.FlxG;
+import flixel.util.FlxSpriteUtil;
+import flixel.util.FlxColor;
+import flixel.FlxCamera;
+import flixel.util.*;
+import openfl.display.*;
+import flixel.*;
+import openfl.display.BitmapData;
+import flixel.group.FlxGroup;
+import flixel.text.FlxText;
+import haxe.ds.Vector;
+import flixel.ui.FlxButton;
+
+class ButtonMenu extends FlxTypedGroup<FlxSprite> {
+	var selector:FlxSprite;
+	var buttons:Array<FlxButton>;
+	var selected:Int = 0;
+	var delay:Float = 0;
+
+	public static function scaleButton(button:FlxButton) {
+		button.scale.set(2, 2);
+		button.label.scale.set(2, 2);
+		button.width *= 2;
+		button.height *= 2;
+	}
+
+	public function new() {
+		super();
+		buttons = new Array<FlxButton>();
+		selector = new FlxSprite(10, 10);
+		selector.makeGraphic(16, 16);
+		add(selector);
+		selector.scrollFactor.set(0, 0);
+		selector.kill();
+	}
+
+	public function updateInput(input:Input) {
+		if (delay <= 0) {
+			var pressed:Bool = false;
+			if (buttons.length > 0) {
+				if (input.directionDown || input.directionRight) {
+					selected += 1;
+					pressed = true;
+				} else if (input.directionUp || input.directionLeft) {
+					selected -= 1;
+					pressed = true;
+				}
+				if (selected < 0) {
+					selected = buttons.length - 1;
+				} else if (selected >= buttons.length) {
+					selected = 0;
+				}
+				if (!pressed && input.confirmPressed) {
+					buttons[selected].onUp.fire();
+					pressed = true;
+				}
+				if (pressed) {
+					selector.revive();
+					selector.y = buttons[selected].y;
+					selector.x = buttons[selected].x - 64;
+				}
+			}
+			if (pressed) {
+				delay = 0.1;
+			}
+		}
+	}
+
+	override public function update(diff:Float) {
+		if (delay > 0) {
+			delay -= diff;
+		}
+		super.update(diff);
+	}
+
+	public function addButton(button:FlxButton) {
+		add(button);
+		buttons.push(button);
+	}
+}
---
+++ source/MainMenu.hx
@@ -0,0 +1,67 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.text.FlxText;
+import flixel.ui.FlxButton;
+import flixel.math.FlxMath;
+import openfl.system.System;
+
+class MainMenu extends FlxState {
+	private var _btnFullScreen:FlxButton;
+	private var buttons:ButtonMenu;
+	private var input:Input = new Input();
+
+	override public function create():Void {
+		FlxG.mouse.visible = true;
+
+		// FlxG.console.autoPause = false;
+		// FlxG.autoPause = false;
+
+		var t1:FlxText = new FlxText(FlxG.width / 2, 10, 200, "Global Game Jam 2019");
+		t1.x -= t1.width / 2;
+		add(t1);
+
+		var t1:FlxText = new FlxText(800, 10, 300, "Authors:\nKarlis, Kristaps");
+		t1.x -= t1.width / 2;
+		add(t1);
+
+		buttons = new ButtonMenu();
+		add(buttons);
+
+		var b1:FlxButton = new FlxButton(FlxG.width / 2, FlxG.height / 2, "Start", function() {
+			FlxG.switchState(new PlayState());
+		});
+		b1.x -= b1.width / 2;
+		buttons.addButton(b1);
+		ButtonMenu.scaleButton(b1);
+
+		#if desktop
+		_btnFullScreen = new FlxButton(FlxG.width / 2, FlxG.height / 2 + 30, FlxG.fullscreen ? "FULLSCREEN" : "WINDOWED", function() {
+			FlxG.fullscreen = !FlxG.fullscreen;
+			_btnFullScreen.text = FlxG.fullscreen ? "FULLSCREEN" : "WINDOWED";
+		});
+
+		_btnFullScreen.x -= _btnFullScreen.width / 2;
+		ButtonMenu.scaleButton(_btnFullScreen);
+		buttons.addButton(_btnFullScreen);
+
+		var _btnExit = new FlxButton(FlxG.width / 2, FlxG.height / 2 + 70, "Exit", function() {
+			System.exit(0);
+		});
+		_btnExit.x -= _btnExit.width / 2;
+		ButtonMenu.scaleButton(_btnExit);
+		buttons.addButton(_btnExit);
+		#end
+
+		var t2:FlxText = new FlxText(10, 110, FlxG.width - 20, "Controls: Gamepad may work\nArrows/WASD/HJKL - move\nSPACE,ENTER - pickup,put down block");
+		add(t2);
+	}
+
+	override public function update(diff:Float) {
+		input.update();
+		buttons.updateInput(input);
+		super.update(diff);
+	}
+}
